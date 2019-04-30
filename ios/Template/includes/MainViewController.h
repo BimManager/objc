d4388118ec5143fc67a49b47bfbdf927993e96cf
BimManager <kirillkozlov@ymail.com>
2019-04-30 11:02:36 -0700

    modified Makefile by using static pattern rules

---
+++ ios/Template/includes/MainViewController.h
@@ -0,0 +1,17 @@
+/*
+ *	MainViewController.h
+ *	APP
+ *	Created by Kirill Kozlov on 4/22/19
+ *	Copyright 2019 Kirill Kozlov. All rights reserved.
+ */
+
+#ifndef MAIN_VIEW_CONTROLLER_H
+# define MAIN_VIEW_CONTROLLER_H
+
+# import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
+
+#endif
---
+++ ios/Template/srcs/MainViewController.m
@@ -0,0 +1,23 @@
+/*
+ *	ViewController.m
+ *	APP
+ *	Created by Kirill Kozlov on 4/22/19
+ *	Copyright 2019 Kirill Kozlov. All rights reserved.
+ */
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+- (void)loadView
+{
+  [super loadView];
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
